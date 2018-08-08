4a51825fa00d23c51e02d02ce7d954f427dad9fd
umamaheshwararao manekar <urao@juniper.net>
2018-08-07 23:04:56 -0700

    Update install-openstack-pkgs.sh

--- install-openstack-pkgs.sh
+++ install-openstack-pkgs.sh
@@ -8,4 +8,6 @@ sudo yum install openstack-utils
 echo "source openrc.sh file"
 source /etc/kolla/kolla-toolbox/admin-openrc.sh
 
-openstack-status
+openstack token issue
+curl -X GET -H "X-Auth-Token:$(openstack token issue | awk '/ id / {print $4}')" \
+      http://<Controller_IP>:8081/analytics/uves/vrouters | python -mjson.tool
