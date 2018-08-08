3ff9a48b907ea80dda6b09e9981510f472822022
umamaheshwararao manekar <urao@juniper.net>
2018-08-07 22:31:38 -0700

    Update install-openstack-pkgs.sh

--- install-openstack-pkgs.sh
+++ install-openstack-pkgs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Install packages"
-yum install -y gcc python-devel
-pip install python-openstackclient
+sudo yum install -y gcc python-devel
+sudo pip install python-openstackclient
+sudo yum install openstack-utils
  
 echo "source openrc.sh file"
 source /etc/kolla/kolla-toolbox/admin-openrc.sh
