0df1a704c118b113f6d46a063c799fd8b952e886
Umamaheshwar M <urao@juniper.net>
2020-07-30 12:40:44 -0700

    Update backup_db.sh

--- backup_db.sh
+++ backup_db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #
-docker exec -i contrail_config_api python /usr/lib/python2.7/site-packages/cfgm_common/db_json_exim.py --export-to db-backup.json
+docker exec -it contrail_config_api /bin/bash 
+python /usr/lib/python2.7/site-packages/cfgm_common/db_json_exim.py --export-to db-backup.json
+
+### Check 
+# https://github.com/urao/tungsten-fabric/blob/master/db_backup_new_container.md
